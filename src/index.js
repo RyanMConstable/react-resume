c8d5e474113d8df85d8b314226dbd2959b0d0e87
RyanMConstable <ryanmconstable@gmail.com>
2023-07-04 02:34:26 -0400

    New static components

--- src/index.js
+++ src/index.js
@@ -3,18 +3,47 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 
-function page(){
+function Page(){
   return (
-    <h1 className="nameHeader">Ryan Constable</h1>
+    <h1 className = "nameHeader">Ryan Constable</h1>
   );
 }
 
 
+function GitHubLinkedIn(){
+  return (
+    <>
+      <a href="https://www.linkedin.com/in/ryan-constable-3b159a214/" rel="external">LinkedIn</a>
+      <br/>
+      <a href="https://github.com/RyanMConstable" rel="external">Github</a>
+    </>
+  )
+}
+
+function Contact(){
+  return (
+    <>
+      <a href="https://RyanMConstable@gmail.com" rel="external">RyanMConstable@gmail.com</a>
+    </>
+  )
+}
+
+
+function Main(){
+  return (
+    <>
+      <div display="inline">
+        <Page/>
+        <GitHubLinkedIn/>
+        <br/>
+        <Contact/>
+      </div>
+    </>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  page()
-);
+root.render(<Main/>);
 
 
 
