7c0a9dfd53e715e38bac54aa220c0fb53edd7c5c
RyanMConstable <ryanmconstable@gmail.com>
2023-07-05 17:18:56 -0400

    Fixed syntax

--- src/InternshipExperience.js
+++ src/InternshipExperience.js
@@ -4,7 +4,7 @@ function InternshipExperience(props){
     let panzuraSummary = "I started off as a Fullstack Developer Intern. After developing my skills in React, mySQL, Node.js, and Express by creating";
     panzuraSummary += " a web application. I was moved to a maintaining team where I wrote Python scripts to automate server cleanup.";
 
-    let tokioSummary = "During my time at Tokio Marine HCC, I designed and created web applications to replace spreadsheet work at the company."
+    let tokioSummary = "During my time at Tokio Marine HCC, I designed and created web applications to replace spreadsheet work at the company.";
 
     return (
         <div>
