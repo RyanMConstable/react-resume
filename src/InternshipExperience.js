fe2e25fde391a2751eca17cd6303e70831494796
RyanMConstable <ryanmconstable@gmail.com>
2023-07-05 17:18:11 -0400

    Fixed summaries

--- src/InternshipExperience.js
+++ src/InternshipExperience.js
@@ -1,10 +1,11 @@
 import Internship from './Internship.js'
 
 function InternshipExperience(props){
-    let panzuraSummary = "I started off as a Fullstack Developer Intern. After developing my skills in React, mySQL, Node.js, and Express I was moved to";
-    panzuraSummary += " a maintaining team where I wrote Python scripts to automate server cleanup."
-    
-    let tokioSummary ="Another internship experience"
+    let panzuraSummary = "I started off as a Fullstack Developer Intern. After developing my skills in React, mySQL, Node.js, and Express by creating";
+    panzuraSummary += " a web application. I was moved to a maintaining team where I wrote Python scripts to automate server cleanup.";
+
+    let tokioSummary = "During my time at Tokio Marine HCC, I designed and created web applications to replace spreadsheet work at the company."
+
     return (
         <div>
             <Internship companyName = "Panzura" title = "Jr. Associate Software Developer" summary={panzuraSummary}/>
