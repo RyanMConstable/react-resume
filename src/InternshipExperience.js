125e664daa8bae197df9d15da4e4141717bd8bae
RyanMConstable <ryanmconstable@gmail.com>
2023-07-05 16:00:28 -0400

    Simple working format for internships/jobs

--- src/InternshipExperience.js
+++ src/InternshipExperience.js
@@ -1,7 +1,7 @@
 import Internship from './Internship.js'
 
 function InternshipExperience(props){
-    let panzuraSummary = "My summary is";
+    let panzuraSummary = "My summary is that I would like to see what happens when I go over and entire line because that would be interesting to see";
     return (
         <div>
             <Internship companyName = "Panzura" title = "Jr. Associate Software Developer" summary={panzuraSummary}/>
