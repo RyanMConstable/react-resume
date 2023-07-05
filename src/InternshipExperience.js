50bf88a9e55ac47ae7969dcc2b2c48a0d8004062
RyanMConstable <ryanmconstable@gmail.com>
2023-07-05 16:04:39 -0400

    Current template for jobs/internships

--- src/InternshipExperience.js
+++ src/InternshipExperience.js
@@ -2,9 +2,11 @@ import Internship from './Internship.js'
 
 function InternshipExperience(props){
     let panzuraSummary = "My summary is that I would like to see what happens when I go over and entire line because that would be interesting to see";
+    let tokioSummary ="Another internship experience"
     return (
         <div>
             <Internship companyName = "Panzura" title = "Jr. Associate Software Developer" summary={panzuraSummary}/>
+            <Internship companyName ="Tokio Marine HCC" title = "Software Developer Intern" summary = {tokioSummary}/>
         </div>
     );
 }
